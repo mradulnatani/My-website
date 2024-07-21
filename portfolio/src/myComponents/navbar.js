30897ed94edf5de2d2c91ea77539fa3c5d397d1e
mradul_09 <mradulnatani0@gmail.com>
2024-07-22 00:27:18 +0530

    changed links

--- portfolio/src/myComponents/navbar.js
+++ portfolio/src/myComponents/navbar.js
@@ -16,8 +16,8 @@ export default function Navbar() {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
       <div className="topnav" id="myTopnav">
         <a href="#" className="active">Home</a>
-        <a href="./about.js">About</a>
-        <a href="./projects.js">Projects</a>
+        <a href="..myComponents/about.js">About</a>
+        <a href=".myComponents/projects.js">Projects</a>
         <a href="#" className="contact-info"><i><b>9302956451 | mradulnatani0@gmail.com</b></i></a>
         <a href="javascript:void(0);" className="icon" onClick={myFunction}>
           <i className="fa fa-bars"></i>
