eb539ddc483e78b83e7930fbe2ad60659925d251
Mradul <130235203+mradulnatani@users.noreply.github.com>
2024-07-29 23:07:32 +0530

    Added a new project

--- portfolio/src/myComponents/projects.js
+++ portfolio/src/myComponents/projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nftlogo from '../myComponents/nftlogo.avif';
 import Blockchainlogo from '../myComponents/blockchainlogo.jpg';
 import Portfoliologo from '../myComponents/portfolio.jpg';
+import AssetManagerLogo from '../myComponents/Asset Manager logo.png'
 import './projcts.css'; 
 
 export default function Projects() {
@@ -58,6 +59,18 @@ export default function Projects() {
               <h5 className="card-title">Portfolio Website</h5>
               <p className="card-text">Portfolio website using Node.js and React.js</p>
               <a href="https://github.com/mradulnatani/My-website">
+                <button className="project-button" onMouseOver={handleButtonOver} onMouseLeave={handleButtonLeave}>Github</button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card" onMouseOver={handleProjectOver} onMouseLeave={handleProjectLeave}>
+            <img src={AssetManagerLogo} className="card-img-top" alt="Blockchain Project" />
+            <div className="card-body">
+              <h5 className="card-title">Asset Manager Pro</h5>
+              <p className="card-text">A comprehensive asset management solution for real-time resource analysis, allocation, and maintenance.</p>
+              <a href="https://github.com/mradulnatani/Pro-Asset-Manager">
                 <button className="project-button" onMouseOver={handleButtonOver} onMouseLeave={handleButtonLeave}>Github</button>
               </a>
             </div>
